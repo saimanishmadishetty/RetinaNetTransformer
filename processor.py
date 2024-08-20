7ad2485a95f540038ae180d18e7ba9f8255f3276
saimanishmadishetty <168367986+saimanishmadishetty@users.noreply.github.com>
2024-08-20 17:03:51 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -6,7 +6,7 @@ import base64
 def pre_process(input):
     image_data = base64.b64decode(input)
     # Open the image and convert to RGB
-    original_image = Image.open(BytesIO(image_data))
+    original_image = Image.open(BytesIO(image_data)).convert('RGB')
     # Convert the image to a tensor
     image = original_image.resize((224, 224))
     # Convert the image to a numpy array and normalize it
